2d362bf0e3e445d6e37c3a7179558d5df17ae944
A-Andrejevic <92856864+A-Andrejevic@users.noreply.github.com>
2021-11-19 13:55:25 +0100

    Integrated API

--- src/Weather.js
+++ src/Weather.js
@@ -1,42 +1,100 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Weather.css";
+import axios from "axios";
 
 export default function Weather() {
+  let [city, setCity] = useState(null);
+  let [location, setLocation] = useState(null);
+  let [temperature, setTemperature] = useState(null);
+  let [description, setDescription] = useState(null);
+  let [humidity, setHumidity] = useState(null);
+  let [wind, setWind] = useState(null);
+  let [icon, setIcon] = useState(null);
+  let [feeling, setFeeling] = useState(null);
+
+window.onload = (event) => {
+  city = "Stockholm";
+  setLocation(`${city}`);
+  let apiKey = "745e466f4597986491c458e2888a3c22";
+  let url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${apiKey}`;
+
+
+  function defaultCity(response)
+  
+  {
+      setTemperature(Math.round(response.data.main.temp));
+      setDescription(response.data.weather[0].description);
+      setWind(response.data.wind.speed);
+      setFeeling(Math.round(response.data.main.feels_like));
+      setHumidity(response.data.main.humidity);
+      setIcon(
+        `https://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
+      );
+    
+
+  }
+  axios.get(url).then(defaultCity);
+};
+
+
+  function submitCity(event) {
+    event.preventDefault();
+    setLocation(`${city}`);
+    let apiKey = "745e466f4597986491c458e2888a3c22";
+    let url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${apiKey}`;
+
+    axios.get(url).then(showCurrentWeather);
+
+    function showCurrentWeather(response) {
+      setLocation(response.data.name);
+      setTemperature(Math.round(response.data.main.temp));
+      setDescription(response.data.weather[0].description);
+      setFeeling(Math.round(response.data.main.feels_like));
+      setWind(response.data.wind.speed);
+      setHumidity(response.data.main.humidity);
+      setIcon(
+        `https://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
+      );
+    }
+  }
+
+  function inputCity(event) {
+    setCity(event.target.value);
+  }
+
   return (
     <div className="Weather">
       <div className="container">
-        <form>
+        <form onSubmit={submitCity}>
           <input
             type="search"
             placeholder="Enter location here"
             id="query-box"
+            onChange={inputCity}
           />
           <input type="submit" value="Search" id="search-button" />
           <button id="location-button">Current location</button>
         </form>
 
-        <h1>Belgrade</h1>
+        <h1>{location}</h1>
         <ul>
           <li>Time</li>
-          <li>Weather description</li>
+          <li className="text-capitalize">{description}</li>
         </ul>
         <div className="row">
           <div className="col-6">
             <div className="clearfix float-left current-weather">
-              <img
-                src="https://ssl.gstatic.com/onebox/weather/64/cloudy.png"
-                alt="cloudy"
-              />
+              <img src={icon} alt={description} />
 
-              <span className="current-temperature">6</span>
+              <span className="current-temperature">{temperature}</span>
               <span className="unit">°C</span>
             </div>
           </div>
           <div className="col-6">
             <ul>
-              <li>Humidity: 5%</li>
-              <li>Wind: 5 km/h</li>
-              <li>Feels like: 5°C</li>
+              <li>Humidity: {humidity}%</li>
+              <li>Wind: {wind} km/h</li>
+              <li>Feels like: {feeling}°C</li>
             </ul>
           </div>
         </div>
@@ -44,3 +102,6 @@ export default function Weather() {
     </div>
   );
 }
+
+
+
